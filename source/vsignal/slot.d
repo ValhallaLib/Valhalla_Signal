2b0ded9e14d0f3708ddf95e6ef60b59e777bbb92
João Lourenço <jlourenco5691@gmail.com>
2021-09-09 18:43:37 +0100

    vsignal.slot: implement function connect
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- source/vsignal/slot.d
+++ source/vsignal/slot.d
@@ -1,5 +1,21 @@
 module vsignal.slot;
 
+package void connect(alias pred, F)(auto ref Slot!F slot)
+{
+	import vsignal.utils : tryForward;
+
+	with(slot)
+	{
+		payload = null;
+		fn = (const scope void*, Parameters args) {
+			static if (is(ReturnType == void))
+				pred(tryForward!(pred, args));
+			else
+				return ReturnType(pred(tryForward!(pred, args)));
+		};
+	}
+}
+
 package struct Slot(F)
 {
 	import vsignal.utils : from;
