c7e39016a22b04ead32f6d7acd1441b6f2bdca05
João Lourenço <jlourenco5691@gmail.com>
2021-09-09 20:21:26 +0100

    vsignal.signal: implement function emit
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- source/vsignal/signal.d
+++ source/vsignal/signal.d
@@ -5,6 +5,13 @@ import vsignal.slot;
 struct Signal(F)
 	if (is(F : RT delegate(Args), RT, Args...))
 {
+	void emit(Slot!F.Parameters args)
+	{
+		import core.lifetime : forward;
+
+		foreach (call; calls)
+			cast(void) call(forward!args);
+	}
 
 package:
 	Slot!F[] calls;
