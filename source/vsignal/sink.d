119affcf6921e54e35339f238d52175a3589d719
João Lourenço <jlourenco5691@gmail.com>
2021-09-10 19:14:10 +0100

    vsignal.sink: implement Sink function disconnect for listeners
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- source/vsignal/sink.d
+++ source/vsignal/sink.d
@@ -3,6 +3,23 @@ module vsignal.sink;
 import vsignal.signal;
 import vsignal.slot : Slot, slot_connect = connect;
 
+void disconnect(alias pred, F)(auto ref Sink!F sink)
+{
+	import std.algorithm.mutation : remove, SwapStrategy;
+	import std.algorithm.searching : countUntil;
+
+	Slot!F call;
+	call.slot_connect!pred;
+
+	with (sink)
+	{
+		const index = signal.calls.countUntil(call);
+
+		if (index > -1)
+			signal.calls = signal.calls.remove!(SwapStrategy.unstable)(index);
+	}
+}
+
 struct Sink(F)
 {
 private:
